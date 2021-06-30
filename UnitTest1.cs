bd7cf67463fe3ef72ec18b1bb8c7fd430da84afc
Osv04 <oscar.veloz4@gmail.com>
2021-06-29 20:18:42 -0400

    a

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,12 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace Kata06_Anagrams
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-
         public void TimeTestThatReturn2seconds()
 
         {
